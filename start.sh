943040973ed474f86dd90ee48e49db19ba00a362
JF <github@itgeek.men>
2020-05-16 23:10:25 +0300

    restructurize

--- start.sh
+++ start.sh
@@ -2,15 +2,22 @@
 
 TOKEN=$(curl -X POST -d "grant_type=client_credentials&client_id=$APP_ID&client_secret=$APP_KEY&resource=https%3A%2F%2Fmanagement.azure.com%2F" https://login.microsoftonline.com/$TENANT_ID/oauth2/token | jq -r '.access_token')
 
-if [ "$METHOD" == 'ARM' ]
-then
- export TIME_STAMP=$(date "+%F")
- result=$(curl -X PUT -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{ 'properties': { 'templateLink': { 'uri': "\"$TEMPLATE_URI\"" }, 'parametersLink': {'uri': "\"$PARAMETERS_URI\""}, 'mode': 'Incremental'}}" https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.Resources/deployments/$TIME_STAMP?api-version=2019-10-01)
+if [ "$METHOD" = "POST" ]; then
+ BODY='Content-Length: 0'
 fi
 
-if [ "$METHOD" == 'POST' ]
-then
- result=$(curl -X $METHOD -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.$RESOURCE_TYPE/$RESOURCE_NAME?api-version=$RESOURCE_API)
+if [ "$METHOD" = "ARM" ]; then
+ BODY="{'properties':{'templateLink':{'uri':'$TEMPLATE_URI'},'parametersLink':{'uri':'$PARAMETERS_URI'},'mode':'Incremental'}}"
+ METHOD='PUT'
+ RESOURCE_NAME=$(date "+%F")
 fi
 
-echo $result | jq '.'
+if [ "$METHOD" = "PUT" ] || [ "$METHOD" = "POST" ]; then
+ result=$(curl -X $METHOD -H "Authorization: Bearer $TOKEN" -H 'Content-Type: application/json' -d $BODY https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.$RESOURCE_TYPE/$RESOURCE_NAME$APPEND_PARAM?api-version=$RESOURCE_API)
+fi
+
+if [ "$METHOD" = "GET" ] || [ "$METHOD" = "DELETE" ]; then
+ result=$(curl -X $METHOD -H "Authorization: Bearer $TOKEN" https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.$RESOURCE_TYPE/$RESOURCE_NAME$APPEND_PARAM?api-version=$RESOURCE_API)
+fi
+
+echo $result | jq -r '.'
